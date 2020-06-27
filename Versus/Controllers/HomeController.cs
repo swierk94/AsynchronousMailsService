06ca521f1bf25986ab292cb1a8c03fdb77848a32
Mateusz Świerczyński <swierk94@wp.pl>
2020-06-27 10:21:25 +0200

    Metoda asynchroniczna

---
+++ Versus/Controllers/HomeController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using Versus.Models;
+using Versus.Models.Data;
+using Versus.ViewModels;
+
+namespace Versus.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region TODO Methods
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ScoresVM model = new ScoresVM
+            {
+                Choices = ScoresGenerate()
+            };
+            return View(model);
+        }
+
+        public ActionResult Test()
+        {
+
+            return View();
+        }
+        #endregion
+
+
+
+
+        // POST: SaveScore
+        [HttpPost]
+        public async Task<ActionResult> SaveScore(ScoresVM model)
+        {
+
+            // Sprawdzenia model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+                // Inicjalizacja PageDTO
+                ScoresDTO dto = new ScoresDTO();
+                --model.Score1;
+                --model.Score2;
+
+                ///////////////////
+                #region TO USE VALIDATION
+                //// gdy niemamy adresu strony to przypisujemy tytul
+                //if (string.IsNullOrWhiteSpace(model.Slug))
+                //{
+                //    slug = model.Title.Replace(" ", "-").ToLower();
+                //}
+                //else
+                //{
+                //    slug = model.Slug.Replace(" ", "-").ToLower();
+                //}
+
+                //// zapobiegamy dodanie takiej samej nazwy strony
+                //if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                //{
+                //    ModelState.AddModelError("", "Ten tytuł lub adres strony już istnieje.");
+                //    return View(model);
+                //}
+                #endregion 
+                ///////////////////
+                
+                dto.Score1 =  model.Score1;
+                dto.Score2 = model.Score2;
+                dto.GameId = model.GameId;
+                dto.BetDate = model.Betdate;
+                dto.UserId = model.UserId;
+
+                // zapis DTO
+                db.Scores.Add(dto);
+                await db.SaveChangesAsync();
+            }
+
+            //utworzenie zmiennej sesyjnej
+
+            Session["score"] = model.Score1.ToString() + " - " + model.Score2.ToString();
+
+            return RedirectToAction("LoadScore");
+        }
+
+        // GET: LoadScore
+        [HttpGet]
+        public ActionResult LoadScore()
+        {
+            //pobranie danych z sesji
+
+            string score =  (string)Session["score"];
+
+            TempData["SM"] = score;
+
+            return View("Test");
+        }
+
+        // GET: ScoresGenerate
+        [HttpGet]
+        public IEnumerable<SelectListItem> ScoresGenerate()
+        {
+           
+
+            List<SelectListItem> choices = new List<SelectListItem>
+               {
+                    new SelectListItem { Text="Wybierz wynik", Value="0"}
+               };
+
+            for (int i = 0; i < 51; i++)
+            {
+                choices.Add
+                    (
+                    new SelectListItem
+                        {
+                            Text = i.ToString(),
+                            Value = (i+1).ToString()
+                        }
+                    );
+            }
+
+            return new SelectList(choices, "Value", "Text");
+        }
+
+    }
+}
---
+++ Versus/ViewModels/ScoresVM.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Versus.Models.Data;
+
+namespace Versus.ViewModels
+{
+
+    public class ScoresVM
+    {
+        public ScoresVM()
+        {
+
+        }
+
+        public ScoresVM(ScoresDTO row)
+        {
+            Id = row.Id;
+            Score1 = row.Score1;
+            Score2 = row.Score2;
+            GameId = row.GameId;
+            Betdate = row.BetDate;
+            UserId = row.UserId;
+        }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int Score1 { get; set; }
+        [Required]
+        public int Score2 { get; set; }
+        // [Required]
+        public string GameId { get; set; }
+        // [Required]
+        public string Betdate { get; set; }
+        // [Required]
+        public int UserId { get; set; }
+
+        public IEnumerable<SelectListItem> Choices { get; set; }
+
+        //[Required]
+        //[StringLength(50, MinimumLength =3)]
+        //[Display(Name = "Tytuł Strony")]
+        //public string Title { get; set; }
+        //[Display(Name = "Adres Strony")]
+        //public string Slug { get; set; }
+        //[Required]
+        //[StringLength(int.MaxValue, MinimumLength = 3)]
+        //[Display(Name = "Zawartość Strony")]
+        //[AllowHtml]
+        //public string Body { get; set; }
+        //public int Sorting { get; set; }
+        //[Display(Name = "Pasek Boczny")]
+        //public bool HasSidebar { get; set; }
+    }
+}
